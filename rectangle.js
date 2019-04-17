b9b85dd00d8376639b7f946040ba2207b1f745d4
wangding <408542507@qq.com>
2019-04-17 14:29:33 +0800

    rectangle

--- rectangle.js
+++ rectangle.js
@@ -1,17 +1,54 @@
-$(function(){
+
+/* global Rectangle, validate: true */
+$(function() {
   var $width = $('#width'),
       $height = $('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
-      $area = $('#area');
-  $btnCal.click(function(){
-    var w = Number($width.val()),
-        h = Number($height.val());
+      $area = $('#area'),
+      $widthValidate = $('#width-validate'),
+      $heightValidate = $('#height-validate'),
+      isPassValidate = false;
 
-    var p = 2*(w+h),
-        a = w*h;
+$width.focusout(function() {
+    var result = validate($width.val());
+        isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $widthValidate.html('宽度' + result.reason);
+      $width.select();
+                            
+    } else {
+      $widthValidate.html('');
+                      
+    }
+            
+});
 
-    $perimeter.val(p);
-    $area.val(a);
-  })
-})
+$height.focusout(function() {
+  var result = validate($height.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $heightValidate.html('高度' + result.reason);
+      $height.select();
+                            
+    } else {
+      $heightValidate.html('');
+                      
+    }
+            
+});
+
+$btnCal.click(function(){
+  if(!isPassValidate) return;
+
+  var w = $width.val(),
+      h = $height.val();
+
+  var r = new Rectangle(w, h);
+
+      $perimeter.val(r.perimeter());
+      $area.val(r.area());
+              
+});
+
+});
